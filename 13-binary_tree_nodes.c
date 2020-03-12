3d2cd8dd003061a560fd68135d38ba1522198d97
Mauricio-KND <1299@holbertonschool.com>
2020-03-12 04:20:28 +0000

    Adding 15 Task. Implementing BHeight

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes - counts nodes with at least 1 child in a binary tree
+ * binary_tree_nodes - Counts nodes with at least 1 child in a binary tree.
  *
- * @tree: pointer to the root node of the tree to count the number of nodes
+ * @tree: Pointer to the root node of the tree to count the number of nodes.
  *
- * Return: number of nodes that are not leaves
+ * Return: 0 if tree is NULL.
  */
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
